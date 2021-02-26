bcf3153312b48bda426e58ce18419610cede6fb0
Paige Morton <paigeashley624@gmail.com>
2021-02-26 15:31:38 -0400

    add todays homework

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,11 +1,7 @@
 class Api::MoviesController < ApplicationController
-  def id(id_number)
-    @actor = Actor.(id_number)
-    render "id.json.jb"
-  end
+  # def id(id_number)
+  #   @actor = Actor.(id_number)
+  #   render "id.json.jb"
+  # end
 
-  def Movies
-    @movies = Movies
-    render "Movies.json.jb"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/Actors" => "actors#id"
+  # end
   namespace :api do
-    get "/Actors" => "actors#id"
-  end
-  namespace :api do
-    get "/Movies" => "movies#new"
+    get "/Movies" => "movies#new_movies"
   end
 end
